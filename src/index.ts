36cb9ae8b42e53d3e5f5f12dc237e20463c6940e
brayansigala20 <brayansigala21@gmail.com>
2026-09-06 12:50:36 -0700

    add prisma/ORM

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.get('/api/hello', (_req, res) => {
   res.json({
-    message: 'Hola desde el backend'
+    message: 'Hola desde el backend desde el servidor prueba 2'
   })
 })
 
